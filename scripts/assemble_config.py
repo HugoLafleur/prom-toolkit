0ecd1d459e308dee2add626d8e420df3be7fd6b0
Hugo Lafleur <handspell@gmail.com>
2019-06-28 15:27:38 +0000

    fix security issue with jinja and pyyaml

--- scripts/assemble_config.py
+++ scripts/assemble_config.py
@@ -5,15 +5,10 @@ from getpass import getuser
 from glob import glob
 from jinja2 import Template
 
-datacenters = yaml.load(
-                  Template(
-                     open('templates/environment.yml.j2').read()
-                  ).render(environment = getuser()))
-
 jobs = list()
 for file in glob('jobs/*'):
    tmpl_job = Template(open(file).read())
-   jobs.append(tmpl_job.render(datacenters = datacenters, dc = 'local'))
+   jobs.append(tmpl_job.render())
 
 tmpl_prom_config = Template(open('templates/prometheus.yml.j2').read())
 prom_config = tmpl_prom_config.render(jobs = jobs)
